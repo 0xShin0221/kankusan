c9cfe58817b9295c691d08b9dcdf538ceb5b6c71
gkrantz <g-u-r-r-a@hotmail.com>
2019-03-21 21:33:15 +0900

    delete .venv

--- backendAPI/.venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/dorichan0629/Desktop/programing/omucrude/backendAPI/.venv/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- backendAPI/.venv/lib/python3.5/linecache.py
+++
@@ -1,1 +0,0 @@
-/Users/dorichan0629/.pyenv/versions/3.5.1/lib/python3.5/linecache.py
